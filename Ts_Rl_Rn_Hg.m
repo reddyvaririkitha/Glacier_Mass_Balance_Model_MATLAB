4dfba1c7149b834becbd8237d1e4f0cf34595b1f
Reddyvari Rikitha <73999899+reddyvaririkitha@users.noreply.github.com>
2021-09-25 18:38:08 +0530

    Update Ts_Rl_Rn_Hg.m

--- Ts_Rl_Rn_Hg.m
+++ Ts_Rl_Rn_Hg.m
@@ -14,15 +14,20 @@ mus = 40;           %extinction coefficient of snow (/m)
 B = 5.67*10^-8;     %stefan-boltzmann constant (W/m^2-K^4)
 di = 900;           %density of ice (kg/m^3)
 
+function Ks = Snow_Thermal_Conductivity(ds)
+    Ks = 0.029( 1 + 0.001*(ds^2))
+end
 
 function Ts = Surface_Temp(Ta,A,Rs,e,Rl,da,U,rh,q,Hg,G_q)
     Ts = Ta + ((1-A)*Rs + e*Rl - e*B*(Ta + 273.2)^4 - le*da*C*U*(1-rh)*q*Ta + Hg)/(4*e*B*(Ta + 273.2)^3 + (G_q*le + ca)*da*C*U);
 end
 
 function Hg = Heat_Into_Glacier(ds,A,Rs,Ts)
-    %if Ts = 0
-    %Hg = 0
-    %else if Ts<0
-    % Hg = -Ks*Ts/bs
-    %else if no water in snow
+    if Ts = 0
+        Hg = 0
+    elseif Ts<0
+        Ks = Snow_Thermal_Conductivity(ds)
+        Hg = -Ks*Ts/bs
+    %elseif no water in snow
     % then integration formula for Hg
+    end
